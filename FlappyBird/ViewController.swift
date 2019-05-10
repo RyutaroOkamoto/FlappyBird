a7ac4831da9d4e03217e0cbb08ebe72f087c9a06
ryutaro <[ryutarouni.0910@gmail.com]>
2019-05-10 21:25:28 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by RYUTARO OKAMOTO on 2019/05/10.
+//  Copyright © 2019 RYUTARO OKAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
